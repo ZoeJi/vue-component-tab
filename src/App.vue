c4010970d5aeaf6a275d7572b117f0c58a87d620
Zoe Qianying Ji <zoe.q.ji@gmail.com>
2021-11-17 13:09:53 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
